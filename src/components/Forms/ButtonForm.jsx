cdc0812b069708accbc5d1e274ceae35b1b66cb9
alvaro <alvarososafrancisco1432@gmail.com>
2026-06-26 02:25:24 +0000

    add vercel for deploy

--- src/components/Forms/ButtonForm.jsx
+++ src/components/Forms/ButtonForm.jsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from "@phosphor-icons/react";
 export const ButtonForm = ({ loadCondition }) => {
   return (
     <button
-      className="cursor-pointer bg-purple-400 mt-4 py-2 rounded-2xl text-black flex justify-center items-center gap-4 border hover:border-purple-400 hover:bg-zinc-950 hover:text-purple-400 transition-colors duration-200 disabled:opacity-50"
+      className="cursor-pointer bg-purple-400 mt-4 py-2 rounded-4xl text-black flex justify-center items-center gap-4 border hover:border-purple-400 hover:bg-zinc-950 hover:text-purple-400 transition-colors duration-200 disabled:opacity-50"
       type="submit"
       disabled={loadCondition}
     >
